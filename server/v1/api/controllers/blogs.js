21cfa51f1a63c7a80d228800bfca25e3661ad500
Innoca <48687109+Innoca@users.noreply.github.com>
2023-02-20 12:30:01 +0200

    New Updates

--- server/v1/api/controllers/blogs.js
+++ server/v1/api/controllers/blogs.js
@@ -1,8 +1,13 @@
-import { find } from '../models/Blog';
+import Blog from '../models/Blog';
+const mongoose = require('mongoose')
 
+import Likes from '../models/Likes'
+
+
+// Collect all blogs
 export function blogs_get_all(req, res, next) {
-    find()
-        .select('blogTitle blogContent _id blogImage')
+    Blog.find()
+        .select('blogTitle blogContent _id blogImage userId')
         .exec()
         .then(docs => {
             const response = {
@@ -12,6 +17,7 @@ export function blogs_get_all(req, res, next) {
                         blogTitle: doc.blogTitle,
                         blogContent: doc.blogContent,
                         blogImage: doc.blogImage,
+                        userId: doc.userId,
                         _id: doc._id,
                         request: {
                             type: 'GET',
@@ -32,4 +38,170 @@ export function blogs_get_all(req, res, next) {
                 error: err
             })
         })
+}
+
+
+// Create a blog
+export function blogs_create_one(req, res, next) {
+    const blog = new Blog({
+        _id: new mongoose.Types.ObjectId(),
+        blogTitle: req.body.blogTitle,
+        blogContent: req.body.blogContent,
+        blogImage: req.file.path,
+        userId: req.body.userId
+    });
+
+    blog.save()
+        
+        .then(result => {
+            console.log(result)
+            res.status(201).json({
+                message: 'Created blog successfully',
+                createdBlog: {
+                    blogTitle: result.blogTitle,
+                    blogContent: result.blogContent,
+                    blogImage: result.blogImage,
+                    userId: result.userId,
+                    _id: result._id,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:2023/blogs/' + result._id
+                    }
+                }
+            })
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
+        });
+}
+
+
+
+// Collect one blog by its id
+export function blogs_get_one(req, res, next) {
+    const id = req.params.blogId;
+    Blog.findById(id)
+        .select('blogTitle blogContent _id blogImage userId')
+        .exec()
+        .then(doc => {
+            console.log(doc)
+            if (doc) {
+                res.status(200).json({
+                    blogTitle: doc,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:2023/blogs/' + doc.id
+                    }
+                })
+            } else {
+                res.status(404).json({
+                    message: "No valid entry found for provided ID"
+                })
+            }
+
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: err })
+        });
+}
+
+
+// Update a blog by its id
+export function blogs_update_one(req, res, next) {
+    const id = req.params.blogId;
+    const updateOps = {};
+  
+    // Expecting JSON object with key-value pairs for blogTitle and blogContent
+    if (req.body.blogTitle) {
+      updateOps.blogTitle = req.body.blogTitle;
+    }
+    if (req.body.blogContent) {
+      updateOps.blogContent = req.body.blogContent;
+    }
+    
+    console.log(updateOps)
+    Blog.findByIdAndUpdate({ _id: id }, { $set: updateOps })
+      .exec()
+      .then((result) => {
+        res.status(200).json({
+          message: "Blog Updated",
+          request: {
+            type: "GET",
+            url: "http://localhost:2023/blogs/" + id,
+          },
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+          error: err,
+        });
+      });
+  }
+  
+  
+
+// Delete a blog by its id
+export function blogs_delete_one(req, res, next)  {
+    const id = req.params.blogId;
+    Blog.deleteOne({ _id: id })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: 'Blog Deleted',
+                request: {
+                    type: 'DELETE',
+                    url: 'http://localhost:2023/blogs',
+                    body: {
+                        blogTitle: 'String', blogContent: 'String'
+                    }
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+}
+
+export function blog_likes(req, res, next) {
+    const id = req.params.blogId
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400)
+        .send({
+            message: "Invalid Blog ID",
+            data: {}
+        });
+    }
+
+    Blog.findOne({ _id: id})
+    .then((blog) => {
+        if(!blog) {
+            return res.status(400).send({
+                message: 'No blog found',
+                data: {}
+            })
+        } else {
+            let current_user = req.user
+
+            blog_likes.findOne ({
+                blogId: blog,
+                userId: userId
+
+            })
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
 }
--- server/v1/api/routes/blog.js
+++ server/v1/api/routes/blog.js
@@ -3,7 +3,13 @@ const router = Router();
 import { Types } from 'mongoose';
 import multer, { diskStorage } from 'multer';
 
-import { blogs_get_all } from '../controllers/blogs';
+import { blogs_create_one, blogs_delete_one, blogs_get_all, blogs_get_one, blogs_update_one } from '../controllers/blogs';
+
+
+
+import checkAuth from '../middleware/check-auth'; 
+
+
 
 const storage = diskStorage({
     destination: function (req, file, cb) {
@@ -36,126 +42,29 @@ import Blog, { findById, deleteOne } from '../models/Blog';
 
 
 
+
+
 // 
-router.get('/', blogs_get_all)
+router.get('/', checkAuth, blogs_get_all)
 
-router.post('/', upload.single('blogImage'), (req, res, next) => {
-    const blog = new Blog({
-        _id: new Types.ObjectId(),
-        blogTitle: req.body.blogTitle,
-        blogContent: req.body.blogContent,
-        blogImage: req.file.path
-    });
+router.get('/:blogId', checkAuth, blogs_get_one)
 
-    blog.save()
-        .then(result => {
-            console.log(result)
-            res.status(201).json({
-                message: 'Created blog successfully',
-                createdBlog: {
-                    blogTitle: result.blogTitle,
-                    blogContent: result.blogContent,
-                    blogImage: result.blogImage,
-                    _id: result._id,
-                    request: {
-                        type: 'GET',
-                        url: 'http://localhost:2010/blogs/' + result._id
-                    }
-                }
-            })
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json({
-                error: err
-            })
-        });
-});
+router.post('/', checkAuth, upload.single('blogImage'), blogs_create_one);
+
+router.patch('/:blogId', checkAuth, blogs_update_one)
 
 // Get by Id
-router.get('/:blogId', (req, res, next) => {
-    const id = req.params.blogId;
-    findById(id)
-        .select('blogTitle blogContent _id blogImage')
-        .exec()
-        .then(doc => {
-            console.log(doc)
-            if (doc) {
-                res.status(200).json({
-                    blogTitle: doc,
-                    request: {
-                        type: 'GET',
-                        url: 'http://localhost:2010/blogs/' + doc.id
-                    }
-                })
-            } else {
-                res.status(404).json({
-                    message: "No valid entry found for provided ID"
-                })
-            }
-
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json({ error: err })
-        });
-});
 
 
 
-router.delete('/:blogId', (req, res, next) => {
-    const id = req.params.blogId;
-    deleteOne({ _id: id })
-        .exec()
-        .then(result => {
-            res.status(200).json({
-                message: 'Blog Deleted',
-                request: {
-                    type: 'DELETE',
-                    url: 'http://localhost:2023/blogs',
-                    body: {
-                        blogTitle: 'String', blogContent: 'String'
-                    }
-                }
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
-        });
-});
+
+router.delete('/:blogId', blogs_delete_one);
 
 
 // // Edit
-// router.patch('/:blogId', checkAuth, (req, res, next) => {
-//     const id = req.params.blogId
-//     const updateOps = {};
-
-//     for (const ops of req.body) {
-//         updateOps[ops.propTitle] = ops.value;
-//     }
-//     Blog.updateMany({ _id: id }, { $set: updateOps})
-//     .exec()
-//     .then(result => {
-//         res.status(200).json({
-//             message: "Blog Updated",
-//             request: {
-//                     type: 'GET',
-//                     url: 'http://localhost:2010/blogs/' + id
-//                 }
-//         })
-//     })
-//     .catch(err => {
-//         console.log(err);
-//         res.status(500).json({
-//             error: err
-//         })
-//     })
-// })
 
 
 
 
-export default router
+
+export default router;
